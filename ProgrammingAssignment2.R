106608469b4c7b4c1d6a456077317f3283a6f113
iwiaGBV <rdcm70@gmail.com>
2022-07-24 16:39:29 -0500

    Update ProgrammingAssignment2.R
    ProgrammingAssignment2

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -2,11 +2,38 @@
 #Write the following functions makeCacheMatrix and cacheSolve
 
 makeCacheMatrix <- function( m = matrix() ) {
-    
-  
+       i <- NULL
+       set <- function( matrix )   {
+              m <<- matrix
+              i <<- NULL
+       }
+       
+       get <- function() {}
+       
+       setInverse <- function(inverse) {
+              i <<- inverse
+       }
+       
+       getInverse <- function() {
+              i
+       }
+       
+       list(set = set, get = get,
+            setInverse = setInverse,
+            getInverse = getInverse)
 }
 
 
 cacheSolve <- function(x, ...) {
-     
+       m <- x$getInverse()
+       
+       if( !is.null(m) ) {
+              message("acquiring cached data")
+              return(m)
+       }
+       
+       data <- x$get()
+       m <- solve(data) %*% data
+       x$setInverse(m)
+       inv
 }
